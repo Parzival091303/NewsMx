5a65c7ee88d92a68bfae760c757dd32c83d91fe3
Erik Obed <erikmenor0913.03@gmail.com>
2026-02-21 17:19:11 -0600

    puerto 4321 a puerto 3000

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+//import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output:'server',
-  adapter:cloudflare(),
+  adapter:node({
+    mode: 'standalone'
+  }),
+  server: {
+    port: process.env.PORT || 3000
+  },
 
   vite: {
     plugins: [tailwindcss()]
